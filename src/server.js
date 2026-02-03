60b283a15b8843c854667e4a990d9dbf21a669e8
Serhii Yemets <Serhiiyemets2020@gmail.com>
2026-02-03 17:08:06 +0100

    server.js commit

--- src/server.js
+++ src/server.js
@@ -1,17 +1,43 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(cors()); // Дозволяє запити з будь-яких джерел
+
+// Логування часу
 app.use((req, res, next) => {
   console.log(`Time: ${new Date().toLocaleString()}`);
   next();
 });
 
+// Маршрут
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello world' });
 });
 
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Something went wrong');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  });
+});
